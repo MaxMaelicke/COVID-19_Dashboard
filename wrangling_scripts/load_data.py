5c69344ff069eb3bdee24bcfae5ec60edd0b43ad
MaxMaelicke <max@maelicke.net>
2020-03-16 08:58:28 +0000

    corrected file path to load pickle data

--- wrangling_scripts/load_data.py
+++ wrangling_scripts/load_data.py
@@ -1,11 +1,11 @@
 import pickle
 
 def load_figures():
-    with open('data/figures.txt', 'rb') as figures_pickle:
+    with open('covidapp/data/figures.txt', 'rb') as figures_pickle:
         figures = pickle.load(figures_pickle)
     return figures
 
 def load_last_date():
-    with open('data/last_date.txt', 'rb') as date_pickle:
+    with open('covidapp/data/last_date.txt', 'rb') as date_pickle:
         last_date = pickle.load(date_pickle)
     return last_date
